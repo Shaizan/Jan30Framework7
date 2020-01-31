0cfc0f99e953feeae04d228d0f35f580592e31df
Kurji <shaizan.kurji@uleth.ca>
2020-01-30 17:54:55 -0700

    Frame work 7 files didn't copy not sure why

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,5 +1,17 @@
 var app = new Framework7({
-    root: '#app'
+    root: '#app',
+    routes: [
+        {
+            path: '/',
+            url: 'index.html'
+        },
+        {
+            path: '/page2/',
+            url: 'pages/page2.html'   
+        }
+    ]
+                         
 })
+
 
 var mainView = app.views.create(".view-main");
